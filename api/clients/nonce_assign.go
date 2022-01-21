e3b180e5ff99415dccafb56f9d6b104616a0a95b
hunjixin <1084400399@qq.com>
2022-01-21 14:56:11 +0800

    add comment for assign nonce

--- api/clients/nonce_assign.go
+++ api/clients/nonce_assign.go
@@ -19,6 +19,10 @@ type nonceAssigner struct {
 func newNonceAssign(full v1api.FullNode) *nonceAssigner {
 	return &nonceAssigner{full: full, lk: sync.Mutex{}}
 }
+
+//AssignNonce assign next nonce for address, in solo mode, should use a seperate address for market message, should save nonce
+//when only connect one daemon, MpoolGetNonce works well, but may have conflict nonce if use multiple daemon behind proxy
+//todo save assgined nonce in local database
 func (nonceAssign *nonceAssigner) AssignNonce(ctx context.Context, addr address.Address) (uint64, error) {
 	nonceAssign.lk.Lock()
 	defer nonceAssign.lk.Unlock()
