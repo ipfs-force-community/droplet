cf3ee1f088b68b5795e741c296e0ad35f7db9967
一页素书 <2931107265@qq.com>
2022-03-18 10:10:20 +0800

    PieceStore: import force-seal deal

---
+++ tools/import-deal/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/filecoin-project/venus-market/tools/import-deal/types"
+)
+
+func ImportDealsToMysql(srcConn, conn string) error {
+	var (
+		maxOpenConn int = 10
+		maxIdleConn int = 10
+	)
+
+	db, err := gorm.Open(mysql.Open(srcConn))
+	if err != nil {
+		return err
+	}
+
+	db.Set("gorm:table_options", "CHARSET=utf8mb4")
+	db = db.Debug()
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = sqlDB.Close()
+	}()
+
+	sqlDB.SetMaxOpenConns(maxOpenConn)
+	sqlDB.SetMaxIdleConns(maxIdleConn)
+	sqlDB.SetConnMaxLifetime(30 * time.Second)
+
+	forceDeals := []*types.ForceDeal{}
+	if err := db.Table("deals").Find(&forceDeals).Error; err != nil {
+		return err
+	}
+
+	// venus-market deals
+	dstDb, err := gorm.Open(mysql.Open(conn))
+	if err != nil {
+		return err
+	}
+
+	dstDb.Set("gorm:table_options", "CHARSET=utf8mb4")
+	dstDb = dstDb.Debug()
+
+	dstSqlDB, err := dstDb.DB()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = dstSqlDB.Close()
+	}()
+
+	dstSqlDB.SetMaxOpenConns(maxOpenConn)
+	dstSqlDB.SetMaxIdleConns(maxIdleConn)
+	dstSqlDB.SetConnMaxLifetime(30 * time.Second)
+
+	deals := make([]types.Deal, 0, len(forceDeals))
+	for _, deal := range forceDeals {
+		deals = append(deals, *deal.ToDeal())
+	}
+
+	if err = dstDb.AutoMigrate(&types.Deal{}); err != nil {
+		return err
+	}
+
+	return dstDb.Create(&deals).Error
+}
+
+func main() {
+	// mysql: user:password@tcp(localhost:3308)/db-name?loc=Local&parseTime=true&innodb_lock_wait_timeout=10
+	var (
+		srcConn, conn string
+	)
+
+	flag.StringVar(&srcConn, "src-conn", "", "mysql conn for src")
+	flag.StringVar(&conn, "conn", "", "mysql conn for market")
+
+	flag.Parse()
+
+	if err := ImportDealsToMysql(srcConn, conn); err != nil {
+		fmt.Printf("import deals to mysql err: %s\n", err.Error())
+		return
+	}
+
+	fmt.Println("import success.")
+}
---
+++ tools/import-deal/types/types.go
@@ -0,0 +1,120 @@
+package types
+
+import (
+	"time"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/ipfs/go-cid"
+
+	"github.com/filecoin-project/venus-messager/models/mtypes"
+
+	"github.com/filecoin-project/venus-market/models/mysql"
+)
+
+const (
+	TTFromForce = "import"
+)
+
+type ForceDeal struct {
+	ID               uint64    `gorm:"primary_key;column:id;type:bigint(20) unsigned AUTO_INCREMENT;not null" json:"id"`
+	Dealid           uint64    `gorm:"column:dealid;unique_index:uq_dealid_ttype;type:bigint(20) unsigned" json:"dealid"`
+	Sectorid         uint64    `gorm:"column:sectorid;type:bigint(20) unsigned;index:idx_sector_id" json:"sectorid"`
+	Offset           uint64    `gorm:"column:offset;type:bigint(20) unsigned" json:"offset"`
+	Unpadsize        uint64    `gorm:"column:unpadsize;type:bigint(20) unsigned" json:"unpadsize"`
+	Filesize         uint64    `gorm:"column:filesize;type:bigint(20) unsigned" json:"filesize"`
+	Finish           bool      `gorm:"column:finish;type:tinyint(3) unsigned" json:"finish"`
+	Start            uint64    `gorm:"column:start;type:bigint(20) unsigned" json:"start"`
+	End              uint64    `gorm:"column:end;type:bigint(20) unsigned" json:"end"`
+	Fetch            bool      `gorm:"column:fetch;type:tinyint(3) unsigned" json:"fetch"`
+	Piececid         string    `gorm:"column:piececid;type:varchar(255)" json:"piececid"`
+	Rootcid          string    `gorm:"column:rootcid;type:varchar(255)" json:"rootcid"`
+	Client           string    `gorm:"column:client;type:varchar(255)" json:"client"`
+	Provider         string    `gorm:"column:provider;type:varchar(255)" json:"provider"`
+	ClientCollateral uint64    `gorm:"column:ccollateral;type:bigint(20) unsigned" json:"ccollateral"`
+	Price            uint64    `gorm:"column:price;type:bigint(20) unsigned" json:"price"`
+	Createtime       time.Time `gorm:"column:createtime;type:timestamp" json:"createtime"`
+	Proposalcid      string    `gorm:"column:proposalcid;type:varchar(255)" json:"proposalcid"`
+	Peerid           string    `gorm:"column:peerid;type:varchar(255)" json:"peerid"`
+	Isabandon        bool      `gorm:"column:isabandon;not null;type:tinyint(3) unsigned" json:"isabandon"`
+}
+
+func convertBigInt(v uint64) mtypes.Int {
+	return mtypes.NewInt(int64(v))
+}
+
+func (fd *ForceDeal) ToDeal() *Deal {
+	pCid, _ := cid.Decode(fd.Piececid)
+	rootCid, _ := cid.Decode(fd.Rootcid)
+	client, _ := address.NewFromString(fd.Client)
+	provider, _ := address.NewFromString(fd.Provider)
+	proposalCid, _ := cid.Decode(fd.Proposalcid)
+
+	md := &Deal{
+		ClientDealProposal: mysql.ClientDealProposal{
+			PieceCID:             mysql.DBCid(pCid),
+			Client:               mysql.DBAddress(client),
+			Provider:             mysql.DBAddress(provider),
+			StartEpoch:           int64(fd.Start),
+			EndEpoch:             int64(fd.End),
+			StoragePricePerEpoch: convertBigInt(fd.Price),
+			ClientCollateral:     convertBigInt(fd.ClientCollateral),
+		},
+		ProposalCid: mysql.DBCid(proposalCid),
+		Client:      fd.Peerid,
+		State:       storagemarket.StorageDealActive,
+		PayloadSize: int64(fd.Filesize),
+		Ref: mysql.DataRef{
+			TransferType: TTFromForce,
+			Root:         mysql.DBCid(rootCid),
+
+			PieceCid: mysql.UndefDBCid,
+		},
+		DealID:       uint64(fd.Dealid),
+		CreationTime: fd.Createtime.UnixNano(),
+		SectorNumber: uint64(fd.Sectorid),
+
+		Offset: fd.Offset,
+	}
+
+	return md
+}
+
+type Deal struct {
+	mysql.ClientDealProposal `gorm:"embedded;embeddedPrefix:cdp_"`
+
+	ProposalCid mysql.DBCid `gorm:"column:proposal_cid;type:varchar(256);primary_key"`
+	AddFundsCid mysql.DBCid `gorm:"column:add_funds_cid;type:varchar(256);"`
+	PublishCid  mysql.DBCid `gorm:"column:publish_cid;type:varchar(256);"`
+	Miner       string      `gorm:"column:miner_peer;type:varchar(128);"`
+	Client      string      `gorm:"column:client_peer;type:varchar(128);"`
+	State       uint64      `gorm:"column:state;type:bigint unsigned;index"`
+
+	PayloadSize           int64         `gorm:"column:payload_size;type:bigint;"`
+	PiecePath             string        `gorm:"column:piece_path;type:varchar(256);"`
+	MetadataPath          string        `gorm:"column:metadata_path;type:varchar(256);"`
+	SlashEpoch            int64         `gorm:"column:slash_epoch;type:bigint;"`
+	FastRetrieval         bool          `gorm:"column:fast_retrieval;"`
+	Message               string        `gorm:"column:message;type:varchar(512);"`
+	FundsReserved         mtypes.Int    `gorm:"column:funds_reserved;type:varchar(256);"`
+	Ref                   mysql.DataRef `gorm:"embedded;embeddedPrefix:ref_"`
+	AvailableForRetrieval bool          `gorm:"column:available_for_retrieval;"`
+
+	DealID       uint64 `gorm:"column:deal_id;type:bigint unsigned;index"`
+	CreationTime int64  `gorm:"column:creation_time;type:bigint;"`
+
+	TransferChannelId mysql.ChannelID `gorm:"embedded;embeddedPrefix:tci_"`
+	SectorNumber      uint64          `gorm:"column:sector_number;type:bigint unsigned;"`
+
+	InboundCAR string `gorm:"column:addr;type:varchar(256);"`
+
+	Offset      uint64 `gorm:"column:offset;type:bigint"`
+	Length      uint64 `gorm:"column:length;type:bigint"`
+	PieceStatus string `gorm:"column:piece_status;type:varchar(128);index"`
+
+	mysql.TimeStampOrm
+}
+
+func (Deal) TableName() string {
+	return "storage_deals"
+}
