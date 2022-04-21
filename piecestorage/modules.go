9213f5cd190757f43873aab4203aa9ccbf773841
hunjixin <1084400399@qq.com>
2022-04-21 15:03:21 +0800

    fix dep

--- piecestorage/modules.go
+++ piecestorage/modules.go
@@ -9,7 +9,7 @@ import (
 var PieceStorageOpts = func(cfg *config.MarketConfig) builder.Option {
 	return builder.Options(
 		// piece
-		builder.Override(new(PieceStorageManager), func(cfg *config.PieceStorage) (*PieceStorageManager, error) {
+		builder.Override(new(*PieceStorageManager), func(cfg *config.PieceStorage) (*PieceStorageManager, error) {
 			return NewPieceStorageManager(cfg)
 		}),
 	)
