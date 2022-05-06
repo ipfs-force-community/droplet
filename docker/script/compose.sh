38caf94f1ee6425ad17a1e16485c6dad4daf5754
tanlang <lin.zexiao@qq.com>
2022-05-06 17:24:59 +0800

    add: docker-compose

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -3,19 +3,16 @@
 echo $@
 
 
-if [ $nettype=="calibnet" ]
-then
-    nettype="cali"
-fi
-
-echo $nettype
-./venus daemon --network=${nettype} --auth-url=http://127.0.0.1:8989 --import-snapshot /snapshot.car
+token=$(cat /env/token )
 
 
-# ./venus-market pool-run \
-# --node-url=/ip4/192.168.200.21/tcp/3454/ \
-# --auth-url=http://192.168.200.21:8989 \
-# --gateway-url=/ip4/192.168.200.21/tcp/45132/ \
-# --messager-url=/ip4/192.168.200.21/tcp/39812/ \
-# --auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiemwiLCJwZXJtIjoiYWRtaW4iLCJleHQiOiIifQ.3u-PInSUmX-8f6Z971M7JBCHYgFVQrvwUjJfFY03ouQ \
-# --piecestorage=fs:/path/pieces_solo
+echo ${token}
+
+
+
+./venus-market pool-run \
+--node-url=/ip4/127.0.0.1/tcp/3453  \
+--auth-url=http://127.0.0.1:8989 \
+--gateway-url=/ip4/127.0.0.1/tcp/45132/ \
+--messager-url=/ip4/127.0.0.1/tcp/39812/ \
+--auth-token=${token}
