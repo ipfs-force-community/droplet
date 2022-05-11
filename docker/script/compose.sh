db0ca0d978a1ebd8af01edb7b54e75fc983dd0b9
tanlang <lin.zexiao@qq.com>
2022-05-11 14:53:34 +0800

    add: more readable log

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -5,10 +5,10 @@ echo $@
 
 token=$(cat /env/token )
 
-
+echo "token:"
 echo ${token}
 
-./venus-market pool-run \
+/app/venus-market pool-run \
 --node-url=/ip4/127.0.0.1/tcp/3453  \
 --auth-url=http://127.0.0.1:8989 \
 --gateway-url=/ip4/127.0.0.1/tcp/45132/ \
