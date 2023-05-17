59357e35c5eeb82ff577177866ca499eff06aee5
tanlang <lin.zexiao@qq.com>
2023-05-17 10:44:50 +0800

    feat: log info when deal was rejected

--- storageprovider/deal_handler.go
+++ storageprovider/deal_handler.go
@@ -602,6 +602,8 @@ func (storageDealPorcess *StorageDealProcessImpl) SendSignedResponse(ctx context
 // StorageDealRejecting(RejectDeal)->StorageDealFailing(FailDeal)
 func (storageDealPorcess *StorageDealProcessImpl) HandleReject(ctx context.Context, deal *types.MinerDeal, event storagemarket.StorageDealStatus, err error) error {
 	storageDealPorcess.eventPublisher.Publish(storagemarket.ProviderEventDealRejected, deal)
+	log.Infof("deal %d rejected: %s", deal.DealID, err)
+
 	deal.State = event
 	deal.Message = err.Error()
 
