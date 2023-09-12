ec74920ab3054489e63a371e4cbc0e853b6949f2
simlecode <69969590+simlecode@users.noreply.github.com>
2023-09-12 15:50:55 +0800

    fix: client start failed

--- config/modules.go
+++ config/modules.go
@@ -470,7 +470,7 @@ var ConfigClientOpts = func(cfg *MarketClientConfig) builder.Option {
 		builder.Override(new(*MarketClientConfig), cfg),
 		builder.Override(new(IHome), cfg),
 		builder.Override(new(*HomeDir), cfg.HomePath),
-		builder.Override(new(*Node), &cfg.Node),
+		builder.Override(new(Node), cfg.Node),
 		builder.Override(new(*Libp2p), &cfg.Libp2p),
 		builder.Override(new(*Signer), &cfg.Signer),
 		builder.Override(new(*Messager), &cfg.Messager),
