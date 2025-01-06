9d70d77d8d34d8f131ddc0a1e659e82d28cc6227
simlecode <69969590+simlecode@users.noreply.github.com>
2025-01-06 16:41:28 +0800

    chore: add debug log

--- retrievalprovider/httpretrieval/bswrap.go
+++ retrievalprovider/httpretrieval/bswrap.go
@@ -22,13 +22,14 @@ type bsWrap struct {
 	dagStoreWrapper stores.DAGStoreWrapper
 }
 
-func newBSWrap(ctx context.Context, dagStoreWrapper stores.DAGStoreWrapper) *bsWrap {
+func newBSWrap(_ context.Context, dagStoreWrapper stores.DAGStoreWrapper) *bsWrap {
 	return &bsWrap{
 		dagStoreWrapper: dagStoreWrapper,
 	}
 }
 
 func (bs *bsWrap) Has(ctx context.Context, blockCID cid.Cid) (bool, error) {
+	log.Infof("bsWrap has %s", blockCID)
 	pieces, err := bs.dagStoreWrapper.GetPiecesContainingBlock(blockCID)
 	if err != nil {
 		return false, err
@@ -38,7 +39,9 @@ func (bs *bsWrap) Has(ctx context.Context, blockCID cid.Cid) (bool, error) {
 }
 
 func (bs *bsWrap) Get(ctx context.Context, blockCID cid.Cid) (blocks.Block, error) {
+	log.Infof("bsWrap get %s", blockCID)
 	pieces, err := bs.dagStoreWrapper.GetPiecesContainingBlock(blockCID)
+	log.Infof("bsWrap get %s %d, err: %v", blockCID, len(pieces), err)
 
 	// Check if it's an identity cid, if it is, return its digest
 	if err != nil {
