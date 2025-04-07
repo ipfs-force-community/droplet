b72ba698d3db8c098508188a140536141b03fd81
simlecode <69969590+simlecode@users.noreply.github.com>
2025-04-07 18:06:38 +0800

    opt: add two commands

--- cli/index.go
+++ cli/index.go
@@ -2,10 +2,14 @@ package cli
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/venus/venus-shared/types"
 	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
+	"github.com/ipni/go-libipni/find/client"
+	"github.com/ipni/go-libipni/find/model"
 	"github.com/multiformats/go-multihash"
 	"github.com/urfave/cli/v2"
 )
@@ -20,6 +24,186 @@ var IndexProvCmd = &cli.Command{
 		indexProvAnnounceLatestHttp,
 		indexProvAnnounceDealRemovalAd,
 		indexProvAnnounceDeal,
+		checkDealIndexCmd,
+	},
+}
+
+var checkDealIndexCmd = &cli.Command{
+	Name:  "check-deal-index",
+	Usage: "Check if a deal is indexed by the index provider",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "miner",
+			Usage: `specify miner address`,
+		},
+		&cli.StringFlag{
+			Name:     "droplet-url",
+			Usage:    "specify the url of the droplet node",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "droplet-token",
+			Usage:    "specify the token of the droplet node",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "node-url",
+			Usage:    "specify the url of the full node",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "node-token",
+			Usage:    "specify the token of the full node",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:  "start",
+			Usage: "check index from this time, eg. 2024-01-01, default is 1 year ago",
+		},
+		&cli.BoolFlag{
+			Name:    "verbose",
+			Aliases: []string{"v"},
+			Usage:   "verbose output",
+		},
+		&cli.StringFlag{
+			Name:  "ipni-url",
+			Value: "https://cid.contact",
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		token := cctx.String("droplet-token")
+		url := cctx.String("droplet-url")
+		nodeURL := cctx.String("node-url")
+		nodeToken := cctx.String("node-token")
+
+		ctx := ReqContext(cctx)
+		nodeAPI, closer, err := DailDropletNode(ctx, url, token)
+		if err != nil {
+			return err
+		}
+		defer closer()
+
+		full, fcloser, err := DailFullNode(ctx, nodeURL, nodeToken)
+		if err != nil {
+			return err
+		}
+		defer fcloser()
+
+		start := time.Now().Add(-365 * 24 * time.Hour)
+		if cctx.IsSet("start") {
+			var err error
+			start, err = time.Parse(time.DateOnly, cctx.String("start"))
+			if err != nil {
+				return fmt.Errorf("invalid start time(%s): %v", cctx.String("start"), err)
+			}
+			fmt.Println("check index from: ", start)
+		}
+		deals, _, err := getDeals(ctx, cctx, nodeAPI, start)
+		if err != nil {
+			return err
+		}
+
+		dealIndexs := make(map[string]*dealIndex)
+		startUnix := start.Unix()
+		verboose := cctx.Bool("verbose")
+
+		ipniURL := cctx.String("ipni-url")
+		cli, err := client.New(ipniURL)
+		if err != nil {
+			return err
+		}
+
+		peerIDs := make(map[string]string)
+		getMinerPeerID := func(miner string) (string, error) {
+			peerID, ok := peerIDs[miner]
+			if ok {
+				return peerID, nil
+			}
+
+			mAddr, err := address.NewFromString(miner)
+			if err != nil {
+				return "", fmt.Errorf("invalid miner address %s: %v", miner, err)
+			}
+			mi, err := full.StateMinerInfo(ctx, mAddr, types.EmptyTSK)
+			if err != nil {
+				return "", fmt.Errorf("get miner %s peer id failed: %v", miner, err)
+			}
+			if mi.PeerId == nil {
+				return "", fmt.Errorf("miner %s peer id is nil", miner)
+			}
+			peerIDs[miner] = mi.PeerId.String()
+
+			return mi.PeerId.String(), nil
+		}
+
+		resCache := make(map[string]*model.FindResponse)
+		fillDealIndex := func(id, miner, payloadCID string) error {
+			di, ok := dealIndexs[miner]
+			if !ok {
+				di = &dealIndex{}
+				dealIndexs[miner] = di
+			}
+			di.dealCount++
+
+			res, ok := resCache[payloadCID]
+			if !ok {
+				result, err := cli.FindByPayloadCID(ctx, payloadCID)
+				if err != nil {
+					return fmt.Errorf("get index by miner %s payload cid %s faield: %v", miner, payloadCID, err)
+				}
+				res = result
+				resCache[payloadCID] = res
+			}
+
+			peerID, err := getMinerPeerID(miner)
+			if err != nil {
+				return err
+			}
+			for _, mrs := range res.MultihashResults {
+				for _, pr := range mrs.ProviderResults {
+					if pr.Provider == nil {
+						continue
+					}
+					if pr.Provider.ID.String() == peerID {
+						di.indexCount++
+						return nil
+					}
+				}
+			}
+			if verboose {
+				fmt.Printf("miner %s deal %s not indexed: %s\n", miner, id, payloadCID)
+			}
+
+			return nil
+		}
+
+		for _, deal := range deals {
+			if deal.CreatedAt < uint64(startUnix) {
+				continue
+			}
+			label, err := deal.Proposal.Label.ToString()
+			if err != nil {
+				fmt.Printf("deal %s label is not string: %v\n", deal.ProposalCid, err)
+				continue
+			}
+			if err := fillDealIndex(deal.ProposalCid.String(), deal.Proposal.Provider.String(), label); err != nil {
+				fmt.Println("fill deal index failed: ", err)
+				break
+			}
+		}
+
+		// todo: get payload cid from dDeals
+		// for _, deal := range dDeals {
+		// 	if deal.CreatedAt < uint64(startUnix) {
+		// 		continue
+		// 	}
+		// }
+
+		for miner, mi := range dealIndexs {
+			fmt.Printf("miner: %s, deal count: %d, index count: %d\n", miner, mi.dealCount, mi.indexCount)
+		}
+
+		return nil
 	},
 }
 
